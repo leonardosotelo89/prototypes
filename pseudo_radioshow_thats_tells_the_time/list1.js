83494c8b3605a737a8636a05aa4a29d77f244fea
leonardosotelo89 <leonardosotelo89@gmail.com>
2025-07-31 05:15:40 -0400

    Create list1.js

---
+++ pseudo_radioshow_thats_tells_the_time/list1.js
@@ -0,0 +1,20 @@
+
+const songs1 = [] 
+songs1[0] = { 
+   intro:['song1intro1.mp3','song1intro2.mp3', 
+          'song1intro3.mp3','song1intro4.mp3'], 
+   song:'firstsong1.mp3', 
+   text:'pais: firstcountry / interprete: first / cancion: song1' 
+ } 
+ songs1[1] = { 
+   intro:['song2intro1.mp3','song2intro2.mp3',
+          'song2intro3.mp3','song2intro4.mp3'], 
+   song:'secondsong2.mp3', 
+   text:'pais: secondcountry / interprete: second / cancion: song2' 
+ } 
+ songs1[2] = { 
+   intro:['song3intro1.mp3','song3intro2.mp3',
+          'song3intro3.mp3','song3intro4.mp3'], 
+   song:'thirdsong3.mp3', 
+   text:'pais: thirdcountry / interprete: third / cancion: song3' 
+ } 
